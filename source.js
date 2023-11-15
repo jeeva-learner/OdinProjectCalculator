dcdb18ec3c927b01616228b10ec0a0ea1a9a784f
Jeevagan <jeevagan12356@gmail.com>
2023-11-15 22:05:56 +0530

    Phase 1 Source code

--- source.js
+++ source.js
@@ -1,42 +1,131 @@
-/* --phase 1*/
-/* Variables */
+/* Phase-1 14 Nov 23 */
+
+// Variables
+
+let FirstOperand = '';
+let SecondOperand = '';
+let Operator = '';
+let Buttons = document.querySelectorAll('button');
 let UserInputArea = document.querySelector('.UserInput');
 let ResultArea = document.querySelector('.Result');
 let Result = '';
-let UserInput = '';
-let Operator = '';
 
-/* Functions */
+// Functions
 
-function NumberHandler(value){
-    if (value.className!=="AC"){
-    UserInput += value.innerText;   
+function ValueHandler(value){
+    if(Operator.length === 0 ){
+        FirstOperand += value.innerText;
     }
-};
+    else if(Operator.length !==0){
+        SecondOperand += value.innerText
+    }
+}
 
 function OperatorHandler(value){
-    Operator = value.innerText;
-};
-
-function UpdateUserInputArea(){
-    if(UserInput.length < 31){
-        UserInputArea.textContent = UserInput;
+    if(Operator.length === 0){
+        Operator = value.innerText;
     }
-};
+    else if(Operator.length !==0 && FirstOperand.length!==0 && SecondOperand.length ===0){
+        Operator = value.innerText;
+    }
+    else if(Operator.length !==0 && FirstOperand.length!==0 && SecondOperand.length!==0){
+        Operate(FirstOperand,SecondOperand,Operator)
+        Operator = value.innerText
+        UserInputArea.textContent = `${Result} ${Operator}`
+        FirstOperand = Result;
+        SecondOperand = '';
+        Result = '';
+    }
+}
 
-/* Execution and data handling */
-buttons = document.querySelectorAll('button');
-buttons.forEach((item) => {
+function UpdateDisplay(){
+        UserInputArea.textContent = `${FirstOperand} ${Operator} ${SecondOperand}`;
+        Result=''
+}
 
-    item.addEventListener('click',()=>{
-        if(item.className == "AC"){
-            UserInput = '';
+function ClearDisplay(){
+    UserInputArea.textContent = '';
+    ResultArea.textContent = '';
+    FirstOperand = '';
+    SecondOperand = '';
+    Operator = '';
+}
+
+function EqualCalculation(){
+    console.log("From equal")
+    if (FirstOperand.length === 0 || SecondOperand.length === 0 || Operator.length === 0){
+        ResultArea.textContent = "Incomplete Inputs"  
+    }
+    else{
+        Operate(FirstOperand,SecondOperand,Operator)
+        ResultArea.textContent = Result.toString()
+        FirstOperand = '';
+        SecondOperand = '';
+        Operator = '';
+        Result = '';
+    }
+}
+
+function Operate(first,second,Operator){
+    a = Number(first)
+    b = Number(second)
+    switch(Operator){
+        case '+':
+            Result += add(a,b)
+            break;
+        case '-':
+            Result += sub(a,b)
+            break;
+        case 'x':
+            Result += mul (a,b)
+            break;
+        case '%':
+            if(b===0){
+                Result = "Error: Division by Zero"
+                break;
+            }
+            else{
+                Result += div(a,b);
+                break;
+            }
+        default:
+            console.log("Error !")
+    }
+}
+
+function add(a,b){
+    return a+b
+}
+
+function sub(a,b){
+    return a-b
+}
+
+function mul(a,b){
+    return a*b
+}
+
+function div(a,b){
+    return a/b
+}
+
+// Operation Flow and event handling
+
+Buttons.forEach((button)=>{
+    button.addEventListener('click',() => {
+    if(['+','-','x','%'].includes(button.innerText)){
+        OperatorHandler(button)
+        UpdateDisplay()
+    }
+    else if(button.innerText == 'AC'){
+        ClearDisplay()
+    }
+    else if(button.className == 'equals'){
+        EqualCalculation()
+    }
+    else{
+        ValueHandler(button)
+        UpdateDisplay()
         }
-        else if(['+','-','*','/','^'].includes(item)){
-            OperatorHandler(item);
-        }
-        NumberHandler(item);
-        UpdateUserInputArea();
-    });
-});
-
+    }) 
+})
