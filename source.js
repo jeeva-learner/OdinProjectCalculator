fe64b29e6437b64ba8ddbfbbab3c8ae97d2def4d
Jeevagan <jeevagan12356@gmail.com>
2023-11-16 22:19:27 +0530

    Updated All the button proerties and handled them accordingly Phase - 1 Completed

--- source.js
+++ source.js
@@ -9,20 +9,34 @@ let Buttons = document.querySelectorAll('button');
 let UserInputArea = document.querySelector('.UserInput');
 let ResultArea = document.querySelector('.Result');
 let Result = '';
+let EqualPressed = false
 
 // Functions
 
 function ValueHandler(value){
     if(Operator.length === 0 ){
-        FirstOperand += value.innerText;
+        if (FirstOperand.includes('.') && value.innerText === '.') {
+            // If the FirstOperand already contains a decimal and the pressed value is also a decimal, do nothing.
+            return;
+        } else if (FirstOperand.length + value.innerText.length <= 31) {
+            FirstOperand += value.innerText;
+        }
     }
     else if(Operator.length !==0){
-        SecondOperand += value.innerText
+        if (SecondOperand.includes('.') && value.innerText === '.'){
+            return
+        }
+        else if(SecondOperand.length + value.innerText.length <=31){
+            SecondOperand += value.innerText;
+        }
     }
 }
 
 function OperatorHandler(value){
-    if(Operator.length === 0){
+    if(Operator.length === 0  && FirstOperand.length === 0){
+        Operator = '' ;
+    }
+    else if(Operator.length === 0){
         Operator = value.innerText;
     }
     else if(Operator.length !==0 && FirstOperand.length!==0 && SecondOperand.length ===0){
@@ -43,6 +57,38 @@ function UpdateDisplay(){
         Result=''
 }
 
+function DelHandler(){
+    if(ResultArea.textContent.length > 0){
+        ResultArea.textContent = '';
+        Result = '';
+    }
+    else {
+        UserInputArea.textContent = UserInputArea.textContent.slice(0,-1)
+        let UserInput = UserInputArea.textContent;
+        let LastOperatorIndex = FindLastOperatorIndex(UserInput);     
+        if (LastOperatorIndex == -1){           // if an operator is not found
+            FirstOperand = UserInput;
+            SecondOperand = '';
+            Operator = '';
+        }
+        else{            // If an Operator is found
+            FirstOperand = UserInput.slice(0,LastOperatorIndex).trim()
+            Operator = UserInput[LastOperatorIndex].trim()
+            SecondOperand = UserInput.slice(LastOperatorIndex+1).trim()
+        }
+    }
+}
+
+function FindLastOperatorIndex(value){
+    const Operators = ['+','-','%','x']
+    for(let i = value.length -1; i>=0; i--){
+        if (Operators.includes(value[i])){
+            return i;
+        }
+    }
+    return -1
+}
+
 function ClearDisplay(){
     UserInputArea.textContent = '';
     ResultArea.textContent = '';
@@ -53,17 +99,21 @@ function ClearDisplay(){
 
 function EqualCalculation(){
     console.log("From equal")
-    if (FirstOperand.length === 0 || SecondOperand.length === 0 || Operator.length === 0){
-        ResultArea.textContent = "Incomplete Inputs"  
-    }
-    else{
-        Operate(FirstOperand,SecondOperand,Operator)
-        ResultArea.textContent = Result.toString()
-        FirstOperand = '';
-        SecondOperand = '';
+    if(EqualPressed){
+        FirstOperand = Result;
         Operator = '';
-        Result = '';
+        SecondOperand = '';
+        equalsPressed = false;
+        UpdateDisplay()
     }
+    else if (FirstOperand.length === 0 || SecondOperand.length === 0 || Operator.length === 0) {
+        ResultArea.textContent = "Incomplete Inputs";
+    }
+    else {
+        Operate(FirstOperand, SecondOperand, Operator);
+        ResultArea.textContent = Result.toString();
+        equalsPressed = true;  // Set the flag after the first equals press
+    }    
 }
 
 function Operate(first,second,Operator){
@@ -122,6 +172,10 @@ Buttons.forEach((button)=>{
     }
     else if(button.className == 'equals'){
         EqualCalculation()
+        UpdateDisplay()
+    }
+    else if(button.className == 'del'){
+        DelHandler()
     }
     else{
         ValueHandler(button)
